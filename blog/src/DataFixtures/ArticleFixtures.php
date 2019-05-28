0e7c998db4e25ee03b83f9618a4caddd3521e784
nawal.zakarya@gmail.com <nawal.zakarya@gmail.com>
2019-05-28 23:23:37 +0200

    Ajout d'une methode slugify pour les titre des articles

--- blog/src/DataFixtures/ArticleFixtures.php
+++ blog/src/DataFixtures/ArticleFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\Article;
+use App\Service\Slugify;
 use Faker;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Bundle\FixturesBundle\Fixture;
@@ -19,12 +20,14 @@ class ArticleFixtures extends Fixture implements DependentFixtureInterface
     public function load(ObjectManager $manager)
     {
         $faker  =  Faker\Factory::create('en_US');
+        $slugify = new Slugify();
 
         for ($i=0; $i <= 50; $i++) {
             $article = new Article();
             $article->setTitle(mb_strtolower($faker->catchPhrase()));
+            $article->setSlug($slugify->generate($article->getTitle()));
             $article->setContent($faker->realText());
-            $article->setCategory($this->getReference('category_' . rand(0, 6)));
+            $article->setCategory($this->getReference('category_' . rand(0,6)));
             $nbRandom = rand(0, 3);
             for ($j = 0; $j <= $nbRandom; $j++) {
                 $article->addTag($this->getReference('tag_' . rand(0, 5)));
