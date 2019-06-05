51f233f69f42864c6628a005296d7d2bb49c5d51
nawal.zakarya@gmail.com <nawal.zakarya@gmail.com>
2019-06-05 20:35:22 +0200

    Add some verification to my Article entity

--- blog/src/Entity/Article.php
+++ blog/src/Entity/Article.php
@@ -5,9 +5,15 @@ namespace App\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\ArticleRepository")
+ * @UniqueEntity(
+ *     fields={"title"},
+ *     message="This title already exists."
+ * )
  */
 class Article
 {
@@ -20,11 +26,18 @@ class Article
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank()
+     * @Assert\Length(max="255")
      */
     private $title;
 
     /**
      * @ORM\Column(type="text")
+     * @Assert\NotBlank()
+     * @Assert\Regex(
+     *     pattern="/(digital)/",
+     *     match=false,
+     *     message="En français, il faut dire numérique")
      */
     private $content;
 
